d6aa84462162f86d264b1d69510d6fb44f436da6
Alex Klick <alexander.klick@credera.com>
2023-07-26 13:26:59 -0600

    update styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import "./App.css";
 import Chat from "./pages/chat/MultiChat/MultiChat";
 import SingleChat from "./pages/chat/SingleChat/SingleChat";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Fab from "./components/FloatingActionButton/FloatingActionButton";
 // import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 // import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import FloatingActionButton from "./components/FloatingActionButton/FloatingActionButton";
+import FloatingAvatar from "./components/FloatingActionButton/ff_text";
+import { FloatingButton } from "./components/FloatingActionButton/Fabtest";
+import Sample from "./components/Chatbot/WebViewer";
 
 function App() {
   const queryClient = new QueryClient({
@@ -18,15 +20,18 @@ function App() {
   })
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <Routes>
           <Route path="/" element={<SingleChat />} />
           <Route path="/many_chats/:models" element={<Chat />} />
           <Route path="/chat/:model" element={<SingleChat />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
       <div style={{position:'absolute', left:'0px', top:'100px'}}>
         <FloatingActionButton />
+        {/* <Sample /> */}
+        {/* <FloatingButton /> */}
+        {/* <FloatingButton /> */}
       </div>
 
     </QueryClientProvider>
--- src/components/Chatbot/ChatMessages/ChatMessages.tsx
+++ src/components/Chatbot/ChatMessages/ChatMessages.tsx
@@ -1,41 +1,105 @@
-import styles from "./ChatMessages.module.css";
-import { FC } from "react";
+import styled, { keyframes } from 'styled-components';
+import { FC, HTMLAttributes } from "react";
 
 interface IMessageProps {
   author: string;
   content: string;
 }
 
+const spin = keyframes`
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+`;
+
+const Spinner = styled.div`
+  border: 16px solid #f3f3f3; /* Light grey */
+  border-top: 16px solid #3498db; /* Blue */
+  border-radius: 50%;
+  width: 120px;
+  height: 120px;
+  animation: ${spin} 2s linear infinite;
+`;
+
+const MessageDiv = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 12px;
+`;
+
+interface MessageContentProps extends HTMLAttributes<HTMLDivElement> {
+  user?: boolean;
+}
+
+const MessageContent = styled.div<MessageContentProps>`
+  padding: 20px;
+  background: ${props => props.user ? '#EDF5FD' : '#ffffff'};
+  border-radius: 8px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.14), 0px 0px 2px rgba(0, 0, 0, 0.12);
+  font-family: "Segoe UI";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 22px;
+  color: #242424;
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+  white-space: pre-wrap;
+  word-wrap: break-word;
+  max-width: 80%;
+`;
+
+const ErrorDiv = styled.div`
+  padding: 12px;
+  border-radius: 8px;
+  box-shadow: rgba(182, 52, 67, 1) 1px 1px 2px, rgba(182, 52, 67, 1) 0px 0px 1px;
+  color: #242424;
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+  max-width: 800px;
+`;
+
+const ErrorMessageContent = styled.span`
+  font-family: "Segoe UI";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 22px;
+  white-space: pre-wrap;
+  word-wrap: break-word;
+  gap: 12px;
+  align-items: center;
+`;
+
 export const UserMessage: FC<IMessageProps> = ({ author, content }) => (
-  <div className={styles.chatMessageUser} tabIndex={0}>
-    <div className={styles.chatMessageUserMessage}>{content}</div>
+  <MessageDiv tabIndex={0}>
+    <MessageContent user>{content}</MessageContent>
     {/* <ChatAvatar message_sender={author} /> */}
-  </div>
+  </MessageDiv>
 );
 
 export const AssistantMessage: FC<IMessageProps> = ({ author, content }) => (
-  <div className={styles.chatMessageGpt} tabIndex={0}>
+  <MessageDiv tabIndex={0}>
+    <MessageContent>{content}</MessageContent>
     {/* <ChatAvatar message_sender={author} /> */}
-    <div className={styles.chatMessageResponse}>{content}</div>
-  </div>
+  </MessageDiv>
 );
 
 export const ErrorMessage: FC<IMessageProps> = ({ content }) => (
-  <div className={styles.chatMessageError}>
-    <div className={styles.chatMessageErrorContent}>
+  <ErrorDiv>
+    <ErrorMessageContent>
       <div
-        className={styles.errorIcon}
         style={{ color: "rgba(182, 52, 67, 1)" }}
       />
       <span>Error</span>
-    </div>
-    <span className={styles.chatMessageErrorContent}>{content}</span>
-  </div>
+    </ErrorMessageContent>
+    <ErrorMessageContent>{content}</ErrorMessageContent>
+  </ErrorDiv>
 );
 
 export const LoadingMessage: FC = () => (
-  <div className={styles.chatMessageGpt}>
-    <div className={styles.spinner}></div>
-  </div>
+  <MessageDiv>
+    <Spinner></Spinner>
+  </MessageDiv>
 );
-
